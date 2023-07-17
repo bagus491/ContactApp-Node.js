2155b2a93e7f1933f2dafb5cb9adce9f15936a28
bagus491 <baguskalam491@gmail.com>
2023-07-17 13:08:17 +0700

    setup awal

---
+++ App.js
@@ -0,0 +1,15 @@
+import Express from 'express'
+const app = Express()
+const port = 3000
+
+
+app.get('/', (req,res) => {
+    res.send('hello world')
+})
+
+
+
+
+app.listen(port,() => {
+    console.log(`server berjalan di port ${port}`)
+})
