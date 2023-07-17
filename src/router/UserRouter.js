89219b6bbb33194a0f2b0c88bcf38f52eceb1ed7
bagus491 <baguskalam491@gmail.com>
2023-07-17 13:23:40 +0700

    selesai menambahkan views set dan use

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const {HomeContacts} = require('../controllers/UserControllers')
 
+const path = require('path')
+app.set(path.join(__dirname, '../views'))
 
-//middleware
+// settings views
+const mainlayouts = require('express-ejs-layouts')
+app.set('views', 'ejs')
+app.use(mainlayouts)
 
 
 app.get('/',HomeContacts)
